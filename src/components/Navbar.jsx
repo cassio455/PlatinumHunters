3eda3f003c8ef7c495ed9323bfb1a658773fffe7
Nina Kushidonti <97530906+Nineikro@users.noreply.github.com>
2025-09-22 21:45:20 -0300

    corrigido Link->NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ function Navbar() {
           {/* Links on the right */}
           <ul className="navbar-nav mx-auto flex-column flex-lg-row text-center">
             <li className="nav-item mx-2">
-              <Link className="nav-link" to="/biblioteca">Biblioteca</Link>
+              <NavLink className="nav-link" to="/biblioteca">Biblioteca</NavLink>
 
             </li>
             <li className="nav-item mx-2">
